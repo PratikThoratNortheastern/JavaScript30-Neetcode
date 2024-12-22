2b191ce9d0525c3a314074f872b8b8a1f029bb75
Pratik Thorat <thorat.p@northeastern.edu>
2024-12-22 10:20:39 -0500

    added comments for revision purpose

--- arrayReduce.js
+++ arrayReduce.js
@@ -6,9 +6,9 @@
  */
 var reduce = function (nums, fn, init) {
   if (nums.length !== 0) {
-    var val = fn(init, nums[0]);
+    var val = fn(init, nums[0]); //returns init since array is empty
     for (let i = 1; i < nums.length; i++) {
-      val = fn(val, nums[i]);
+      val = fn(val, nums[i]); //this is when array is not empty.
     }
     return val;
   }
